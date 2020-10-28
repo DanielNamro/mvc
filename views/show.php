496aa0c2817164f2c0c3fb6a4f2b30dabe623b9b
DanielNamro <danielnamrofpb@gmail.com>
2020-10-28 17:11:44 +0000

    eo

---
+++ views/show.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>Usuario numero <?= $user[0] ?></li>
+        <li>Nombre: <?= $user[1] ?></li>
+    </ul>
+</body>
+</html>
